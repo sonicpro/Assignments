2c48b9b9b335038aaa01f8786abe1310f7c1d775
Volodymyr Tsurko <chegnenado@yahoo.com>
2020-03-21 16:22:18 +0200

    Another way to revert a linked list recursively.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ll = new Entry("1")
+            {
+                Next = new Entry("2")
+                {
+                    Next = new Entry("3")
+                }
+            };
+            var revertedList = Revert(ll);
+            var head = revertedList;
+            while (head != null)
+            {
+                Console.WriteLine(head.Value);
+                head = head.Next;
+            }
+        }
+
+        static Entry Revert(Entry current)
+        {
+            if (current.Next == null)
+            {
+                return current;
+            }
+            else
+            {
+                var newHead = Revert(current.Next);
+                current.Next.Next = current;
+                current.Next = null;
+                return newHead;
+            }
+        }
+    }
+
+    public class Entry
+    {
+        public string Value { get; }
+
+        public Entry Next { get; set; }
+
+        public Entry(string value)
+        {
+            Value = value;
+        }
+    }
+}
